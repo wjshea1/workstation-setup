8898e3351c2c007d77b98d2e73a6786e9e320472
wjshea1 <wjshea@mac.com>
2016-04-14 14:39:57 -0400

    Update java.sh

--- scripts/java.sh
+++ scripts/java.sh
@@ -2,8 +2,9 @@ echo
 echo "Installing Java Development tools"
 brew cask install java
 brew cask install intellij-idea
+brew cask install clion
+brew cask install android-studio
 brew install maven
 brew install gradle
 brew install nexus
-brew install jenkins
-brew install springboot
+
