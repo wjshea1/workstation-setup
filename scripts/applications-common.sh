556c78ffc5c8cef2d905a654f40cd4c58eea2960
wjshea1 <wjshea@mac.com>
2016-04-14 14:35:10 -0400

    Update applications-common.sh

--- scripts/applications-common.sh
+++ scripts/applications-common.sh
@@ -8,7 +8,7 @@ brew cask install flycut
 brew cask install shiftit
 brew cask install dash
 brew cask install postman
-brew cask install google-drive
+
 
 # Terminals
 
@@ -17,13 +17,13 @@ brew cask install iterm2
 # Browsers
 
 brew cask install google-chrome
-brew cask install firefox
+
 
 # Communication
 
-brew cask install slack
+
 brew cask install screenhero
-brew cask install skype
+
 
 # Text Editors
 
